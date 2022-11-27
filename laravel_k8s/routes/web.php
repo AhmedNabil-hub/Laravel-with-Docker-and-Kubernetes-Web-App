4cd455c2a1de2eabf5208501d2ffa93d0d316683
Ahmed Nabil <a7med.nabil1655@gmail.com>
2022-11-27 16:54:27 +0200

    Docker compose app

--- laravel_k8s/routes/web.php
+++ laravel_k8s/routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
+});
+
+Route::get('/info', function() {
+	return response([
+		'DB_HOST' => env('DB_HOST'),
+		'DB_CONNECTION' => env('DB_CONNECTION'),
+		'DB_DATABASE' => env('DB_DATABASE'),
+		'DB_PORT' => env('DB_PORT'),
+		'DB_USERNAME' => env('DB_USERNAME'),
+		'DB_PASSWORD' => env('DB_PASSWORD'),
+	]);
 });
